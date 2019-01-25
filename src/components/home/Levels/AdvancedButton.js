5b2d565413760efa36cae85b41a8571d44132fb0
Blake Fletcher <blkfltchr@gmail.com>
2019-01-25 15:35:10 -0500

    Changed home page buttons to dead links for intermediate and advanced

--- src/components/home/Levels/AdvancedButton.js
+++ src/components/home/Levels/AdvancedButton.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 
 const AdvancedButton = () => {
     return ( 
-        <div>
-            <Link to='/beginner' style={{color: 'grey', textDecoration: 'none'}}>
-                <div className='box'>
-                    <b style={{color: 'blue', textDecoration: 'underline'}}>Advanced</b>
-                    <p>You are very comfortable with the language, you enjoy talking about complex subjects, and you want a real challenge.</p>
-                </div>
-            </Link>
+        <div style={{color: 'grey', textDecoration: 'none'}}>
+            <div className='box'>
+                <p><b style={{textDecoration: 'underline'}}>Advanced</b><br/>(Coming soon...)</p>
+                <p>You are very comfortable with the language, you enjoy talking about complex subjects, and you want a real challenge.</p>
+            </div>
         </div>
      );
 }
