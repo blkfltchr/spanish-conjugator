797efe35989cd30ec02e1f816b9861eae6938448
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-26 17:41:56 -0500

    Added link to random page on options page

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Home from './components/home/Home';
 import Header from './components/Header';
 import ConjugatorContainer from './components/conjugatorPage/ConjugatorContainer';
 import Conversation from './conversationPage/Conversation';
-import { spainSpanish, latamSpanish } from './components/NumPersonFilters';
-import { beginner, intermediate } from './components/VerbTensesFilters';
 
 class App extends Component {
   constructor() {
@@ -19,57 +17,7 @@ class App extends Component {
       VerbTenses: 'Intermediate'
     };
   }
-
-  filterData = event => {
-    event.preventDefault();
-
-    if (this.state.VerbTenses === 'Beginner') {
-      this.setState({
-        data: beginner
-      });
-    }
-
-    if (this.state.VerbTenses === 'Intermediate') {
-      this.setState({
-        data: intermediate
-      });
-    }
-
-    if (this.state.VerbTenses === 'Advanced') {
-      this.setState({
-        data: miniData
-      });
-    }
-
-    const spainSpanishFiltered = spainSpanish(this.state.data);
-    if (this.state.NumberPerson === 'Spain') {
-      this.setState({
-        tempData: spainSpanishFiltered
-      });
-    }
-    const latamFiltered = latamSpanish(this.state.data);
-    if (this.state.NumberPerson === 'Latam') {
-      this.setState({
-        tempData: latamFiltered
-      });
-    }
-  };
-
-  updateNumPerson = event => {
-    this.setState({
-      NumberPerson: event.target.value
-    });
-  };
-
-  updateVerbTenses = event => {
-    this.setState({
-      VerbTenses: event.target.value
-    });
-  };
-
   render() {
-    console.log('Data =', this.state.data);
-    console.log('Temp data =', this.state.tempData);
     return (
       <div style={{ width: '420px', margin: '0 auto' }}>
         <Route path="/" component={Header} />
@@ -87,18 +35,7 @@ class App extends Component {
           )}
         />
         <div>
-          <Route
-            exact
-            path="/options"
-            render={props => (
-              <OptionsContainer
-                {...props}
-                updateNumPerson={this.updateNumPerson}
-                updateVerbTenses={this.updateVerbTenses}
-                filterData={this.filterData}
-              />
-            )}
-          />
+          <Route exact path="/options" component={OptionsContainer} />
         </div>
       </div>
     );
--- src/components/optionsPage/OptionsContainer.js
+++ src/components/optionsPage/OptionsContainer.js
@@ -1,35 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
+import miniData from '../../miniData';
 import VerbFrequency from './VerbFrequency';
 import VerbTenses from './VerbTenses';
 import NumberPerson from './NumberPerson';
 import RandomButton from '../home/RandomButton';
 import ConversationButton from '../home/ConversationButton';
+import { spainSpanish, latamSpanish } from '../../components/NumPersonFilters';
+import { beginner, intermediate } from '../../components/VerbTensesFilters';
+import { Link } from 'react-router-dom';
 
-const OptionsContainer = props => {
-  return (
-    <div>
-      <form onSubmit={props.filterData}>
-        <button type="submit">Start</button>
-      </form>
+class OptionsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: miniData,
+      tempData: [],
+      NumberPerson: 'Latam',
+      VerbTenses: 'Intermediate'
+    };
+  }
+
+  filterData = event => {
+    event.preventDefault();
+
+    if (this.state.VerbTenses === 'Beginner') {
+      this.setState({
+        data: beginner
+      });
+    } else if (this.state.VerbTenses === 'Intermediate') {
+      this.setState({
+        data: intermediate
+      });
+    } else if (this.state.VerbTenses === 'Advanced') {
+      this.setState({
+        data: miniData
+      });
+    }
+
+    const spainSpanishFiltered = spainSpanish(this.state.data);
+    if (this.state.NumberPerson === 'Spain') {
+      this.setState({
+        tempData: spainSpanishFiltered
+      });
+    }
+    const latamFiltered = latamSpanish(this.state.data);
+    if (this.state.NumberPerson === 'Latam') {
+      this.setState({
+        tempData: latamFiltered
+      });
+    }
+  };
+
+  updateNumPerson = event => {
+    this.setState({
+      NumberPerson: event.target.value
+    });
+  };
+
+  updateVerbTenses = event => {
+    this.setState({
+      VerbTenses: event.target.value
+    });
+  };
+
+  render() {
+    console.log('Data =', this.state.data);
+    console.log('Temp data =', this.state.tempData);
+    return (
       <div>
-        <h3>Verb Frequency</h3>
-        <VerbFrequency />
+        <form onSubmit={this.filterData}>
+          <Link to="/random">
+            <button type="submit">Start</button>
+          </Link>
+        </form>
+        <div>
+          <h3>Verb Frequency</h3>
+          <VerbFrequency />
+        </div>
+        <div>
+          <h3>Verb Tenses</h3>
+          <VerbTenses updateVerbTenses={this.updateVerbTenses} />
+        </div>
+        <div>
+          <h3>Number and Person</h3>
+          <NumberPerson updateNumPerson={this.updateNumPerson} />
+        </div>
+        <RandomButton />
+        <ConversationButton />
       </div>
-      <div>
-        <h3>Verb Tenses</h3>
-        <VerbTenses
-          setTenses={props.setTenses}
-          updateVerbTenses={props.updateVerbTenses}
-        />
-      </div>
-      <div>
-        <h3>Number and Person</h3>
-        <NumberPerson updateNumPerson={props.updateNumPerson} />
-      </div>
-      <RandomButton />
-      <ConversationButton />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default OptionsContainer;
