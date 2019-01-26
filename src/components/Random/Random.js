42784fa9cb8817041376deee654f2cfc0011e5cc
Blake Fletcher <blkfltchr@gmail.com>
2019-01-26 17:21:07 -0500

    Added _redirects file

--- src/components/Random/Random.js
+++ src/components/Random/Random.js
@@ -7,7 +7,6 @@ import ConversationButton from '../home/Options/ConversationButton';
 const Random = (props) => {
     return ( 
       <div>
-        <h3>Based on the verb, tense, and form...<br /> submit your answer below.</h3>
         <BeginnerRandom data={props.data} />
         <OptionsButton />
         <ConversationButton />
