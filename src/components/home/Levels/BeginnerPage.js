0a43756855672eab868cd13d744ab959a850721c
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-03 18:50:17 -0500

    Fixed merge conflicts v2

--- src/components/home/Levels/BeginnerPage.js
+++ src/components/home/Levels/BeginnerPage.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import RandomButton from '../Options/RandomButton'
-import OptionsButton from '../Options/OptionsButton'
-<<<<<<< HEAD
-=======
-import ConversationButton from '../Options/ConversationButton'
->>>>>>> 730725da352327dfdac94baed8bedfaca72b0bb9
+import RandomButton from '../Options/RandomButton';
+import OptionsButton from '../Options/OptionsButton';
 
 const BeginnerPage = () => {
-    return ( 
-        <div>
-            <p><b>What would you like to do?</b></p>
-            <RandomButton />
-            <OptionsButton />
-        </div>
-     );
-}
- 
+  return (
+    <div>
+      <p>
+        <b>What would you like to do?</b>
+      </p>
+      <RandomButton />
+      <OptionsButton />
+    </div>
+  );
+};
+
 export default BeginnerPage;
