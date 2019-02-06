98937d8eb184d39a2114e1325fb13130c2266e5d
Blake Fletcher <blkfltchr@gmail.com>
2019-02-06 11:30:11 -0500

    Changed routing so /random is / now

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {Route} from "react-router-dom";
 import beginnerData from "./beginnerData";
 
 import Header from "./components/Header";
-import Home from "./components/home/Home";
 import BeginnerPage from "./components/home/Levels/BeginnerPage";
 
 import AdvancedOptions from "./components/optionsPage/AdvancedOptions";
@@ -22,13 +21,12 @@ class App extends Component {
     return (
         <div style={ {width: "420px", margin: "0 auto"} }>
             <Route path="/" component={ Header } />
-            <Route exact path="/" component={ Home } />
             <Route exact path="/beginner" component={ BeginnerPage } />
             <Route path="/options" component={ OptionsContainer } />
             <Route path="/advancedOptions" component={ AdvancedOptions } />
             <Route
           exact
-          path="/random"
+          path="/"
           render={ props => <Random { ...props } data={ this.state.beginnerData } /> }
         />
         </div>
