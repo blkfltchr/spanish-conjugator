4087d92d612393e21eea38b419a1886ee5c256c1
Blake Fletcher <blkfltchr@gmail.com>
2019-02-06 11:54:36 -0500

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Route} from "react-router-dom";
 
 import Header from "./components/Header";
-import BeginnerPage from "./components/home/Levels/BeginnerPage";
 
 import AdvancedOptions from "./components/optionsPage/AdvancedOptions";
 import OptionsContainer from "./components/optionsPage/OptionsContainer";
@@ -12,11 +11,9 @@ const App = () => {
     return (
         <div style={ {width: "420px", margin: "0 auto"} }>
             <Route path="/" component={ Header } />
-            <Route exact path="/beginner" component={ BeginnerPage } />
             <Route path="/options" component={ OptionsContainer } />
             <Route path="/advancedOptions" component={ AdvancedOptions } />
             <Route exact path="/" component={ Random } /> 
-        />
         </div>
     );
   }
