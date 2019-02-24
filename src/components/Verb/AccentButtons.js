a383c5869b59c3456dba73a8d1c88be17945cdc2
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-24 11:28:54 -0500

    Updated accent buttons so that theyre not clicked on pressing enter

--- src/components/Verb/AccentButtons.js
+++ src/components/Verb/AccentButtons.js
@@ -3,22 +3,22 @@ import React from 'react';
 const AccentButtons = props => {
   return (
     <div>
-      <button onClick={props.addAccent} value="á">
+      <button onClick={props.addAccent} type="button" value="á">
         á
       </button>
-      <button onClick={props.addAccent} value="é">
+      <button onClick={props.addAccent} type="button" value="é">
         é
       </button>
-      <button onClick={props.addAccent} value="í">
+      <button onClick={props.addAccent} type="button" value="í">
         í
       </button>
-      <button onClick={props.addAccent} value="ñ">
+      <button onClick={props.addAccent} type="button" value="ñ">
         ñ
       </button>
-      <button onClick={props.addAccent} value="ó">
+      <button onClick={props.addAccent} type="button" value="ó">
         ó
       </button>
-      <button onClick={props.addAccent} value="ú">
+      <button onClick={props.addAccent} type="button" value="ú">
         ú
       </button>
     </div>
