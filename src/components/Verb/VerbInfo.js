ab61ef66f19469d152f0967cbd396142abaf447c
Blake Fletcher <blkfltchr@gmail.com>
2019-02-24 14:49:04 -0500

    Changed font weight to 800

--- src/components/Verb/VerbInfo.js
+++ src/components/Verb/VerbInfo.js
@@ -4,9 +4,9 @@ const VerbInfo = props => {
   return (
     <div style={{backgroundColor: 'white', padding: '15px 20px', borderRadius: '20px'}}>
       <div style={{fontSize: '12px'}}>Verb:</div>
-      <div style={{fontSize: '24px', textTransform: 'uppercase'}}>{props.infinitive}</div>
+      <div style={{fontSize: '24px', textTransform: 'uppercase', fontWeight: '800'}}>{props.infinitive}</div>
       <div style={{fontSize: '12px'}}>Tense:</div>
-      <div style={{fontSize: '24px', textTransform: 'uppercase'}}>{props.tense_english} {props.mood_english}</div>
+      <div style={{fontSize: '24px', textTransform: 'uppercase', fontWeight: '800'}}>{props.tense_english} {props.mood_english}</div>
     </div>
   );
 };
--- src/components/Verb/VerbStreak.js
+++ src/components/Verb/VerbStreak.js
@@ -5,11 +5,11 @@ const VerbStreak = props => {
     <div style={{display: 'flex', justifyContent: 'center'}}>
       <div style={{backgroundColor: 'white', padding: '5px 10px', borderRadius: '10px', margin: '10px', width: '90px'}}>
         <div style={{fontSize: '12px'}}>current streak:</div>
-        <div style={{fontSize: '24px'}}>{props.count}</div>
+        <div style={{fontSize: '24px', fontWeight: '800'}}>{props.count}</div>
       </div>
       <div style={{backgroundColor: 'white', padding: '5px 10px', borderRadius: '10px', margin: '10px', width: '90px'}}>
         <div style={{fontSize: '12px'}}>best streak:</div>
-        <div style={{fontSize: '24px'}}>{props.bestStreak}</div>
+        <div style={{fontSize: '24px', fontWeight: '800'}}>{props.bestStreak}</div>
       </div>
     </div>
   );
