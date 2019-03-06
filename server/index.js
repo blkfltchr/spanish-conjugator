f708525e24dbbe84c3f94f7219e2ea921775d38d
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-03-06 15:27:15 -0500

    Updated node modules

--- server/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const server = require('./server');
-
-const PORT = process.env.PORT || 9000;
-
-server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
