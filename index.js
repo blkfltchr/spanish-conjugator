e96a36505f1367014656ac0c54338416d4b1eb5d
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-04-15 17:48:58 -0400

    routes, register, log in, server setup

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const server = express();
-server.use(express());
-
-
-
+const { server } = require('./server.js');
 const port = process.env.PORT || 4000;
-server.listen(port, () => console.log(`server rolling on port ${port}`));
+
+
+server.listen(port, () => {
+  console.log(`\n=== Server listening on port ${port}\n`);
+});
