c65a69a19cd565e9680cbe35fcbeba9f4e2f35dd
CodyBrewer <brewer.cody.m@gmail.com>
2019-04-25 21:17:29 -0600

    set up for Contribution Graph

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Layout/Header';
+import Header from "./components/Layout/Header";
+import Container from "./components/Verb/index";
+import Graph from "./components/Graph/Graph";
 
-import Container from './components/Verb/index';
-
-
-function App () {
-  return(
-      <div className="app">
-        <Header />
-        <div className="app-wrapper">
-          <Container />
-        </div>
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <div className="app-wrapper">
+        <Container />
+        <Graph />
       </div>
+    </div>
   );
 }
 
---
+++ src/components/Graph/Graph.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./graph.css";
+
+const Graph = () => {
+  return (
+    <div>
+      <h1>i will be a graph</h1>
+    </div>
+  );
+};
+
+export default Graph;
