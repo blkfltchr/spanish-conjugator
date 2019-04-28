fd67517f7e774a64cf589b58214590e124fac272
JustinAndrade <j.andrade.john@gmail.com>
2019-04-28 00:44:16 -0400

    new data pulled for tooltip

--- src/components/Verb/Info.js
+++ src/components/Verb/Info.js
@@ -4,7 +4,7 @@ import ReactTooltip from 'react-tooltip';
 
 const Info = (props) => {
 	console.log(props);
-	const { infinitive, infinitive_english, tense_english, mood_english } = props;
+	const { infinitive, infinitive_english, tense_english, mood_english, verb_english } = props;
 	return (
 		<div className="verb-info">
 			<div className="twelve">Verb:</div>
@@ -17,7 +17,7 @@ const Info = (props) => {
 			<div className="tense" data-tip="React-tooltip">
 				{tense_english} {mood_english === 'Subjunctive' ? `(${mood_english})` : null}
 				<ReactTooltip place="right" type="info" effect="float">
-					{tooltip(tense_english)}
+					{tooltip(tense_english, verb_english)}
 				</ReactTooltip>
 			</div>
 		</div>
@@ -28,65 +28,15 @@ Info.propTypes = {
 	infinitive: PropTypes.string,
 	infinitive_english: PropTypes.string,
 	tense_english: PropTypes.string,
-	mood_english: PropTypes.string
+	mood_english: PropTypes.string,
+	verb_english: PropTypes.string
 };
 
 export default Info;
 
-const tooltip = (value) => {
-	if (value === 'Present') {
-		return 'Present example';
-	}
-	if (value === 'Future') {
-		return 'Future example';
-	}
-	if (value === 'Imperfect') {
-		return 'Imperfect';
-	}
-	if (value === 'Preterite') {
-		return 'Preterite';
-	}
-	if (value === 'Conditional') {
-		return 'Conditional';
-	}
-	if (value === 'Present Perfect') {
-		return 'Present Perfect';
-	}
-	if (value === 'Future Perfect') {
-		return 'Future Pefect';
-	}
-	if (value === 'Past Perfect') {
-		return 'Past Perfect';
-	}
-	if (value === 'Preterite (Archaic)') {
-		return 'Preterite';
-	}
-	if (value === 'Conditional Perfect') {
-		return 'Conditional Perfect';
-	}
-	if (value === 'Present') {
-		return 'Present';
-	}
-	if (value === 'Imperfect') {
-		return 'Imperfect';
-	}
-	if (value === 'Future') {
-		return 'Future';
-	}
-	if (value === 'Present Perfect') {
-		return 'Present Perfect';
-	}
-	if (value === 'Future Perfect') {
-		return 'Future Perfect';
-	}
-	if (value === 'Past Perfect') {
-		return 'Past Perfect';
-	}
-	if (value === 'Affirmative Present') {
-		return 'Affirmative Present';
-	}
-	if (value === 'Negative Present') {
-		return 'Negative Present';
+const tooltip = (value, tense) => {
+	if (value) {
+		return tense;
 	} else {
 		return 'ERROR';
 	}
--- src/components/Verb/Input.js
+++ src/components/Verb/Input.js
@@ -1,181 +1,153 @@
-import React, { useState, useRef } from "react";
-import Reward from "react-rewards";
-import PropTypes from "prop-types";
-import Person from "./Person";
-import AccentButtons from "./AccentButtons";
-import Info from "./Info";
-import Checkmark from "../Checkmark/Checkmark";
+import React, { useState, useRef } from 'react';
+import Reward from 'react-rewards';
+import PropTypes from 'prop-types';
+import Person from './Person';
+import AccentButtons from './AccentButtons';
+import Info from './Info';
+import Checkmark from '../Checkmark/Checkmark';
 
-const Input = props => {
-  const [value, setValue] = useState("");
-  const [bestStreak, setBestStreak] = useState(0);
-  const [totalAnswers, setTotalAnswers] = useState(0);
-  const [correctAnswers, setCorrectAnswers] = useState(0);
-  const [answered, setAnswered] = useState(false);
-  const [helperText, setHelperText] = useState(null);
-  const [correct, setCorrect] = useState(false);
-  const refReward = useRef(null);
+const Input = (props) => {
+	const [ value, setValue ] = useState('');
+	const [ bestStreak, setBestStreak ] = useState(0);
+	const [ totalAnswers, setTotalAnswers ] = useState(0);
+	const [ correctAnswers, setCorrectAnswers ] = useState(0);
+	const [ answered, setAnswered ] = useState(false);
+	const [ helperText, setHelperText ] = useState(null);
+	const [ correct, setCorrect ] = useState(false);
+	const refReward = useRef(null);
 
-  const handleChange = event => {
-    setCorrect(false);
-    setValue(event.target.value);
-  };
+	const handleChange = (event) => {
+		setCorrect(false);
+		setValue(event.target.value);
+	};
 
-  const handleSubmit = event => {
-    const { randomPerson, addCounter, resetCounter } = props;
-    event.preventDefault();
-    const userInput = value.toLowerCase();
-    if (answered === true) {
-      setTotalAnswers(totalAnswers + 1);
-      handleRefresh();
-      setAnswered(false);
-    } else if (randomPerson[1] === userInput) {
-      addCounter();
-      setCorrectAnswers(correctAnswers + 1);
-      setTotalAnswers(totalAnswers + 1);
-      // alert('Correct!')
-      handleRefresh();
-      setCorrect(true);
-      addStreak();
-    } else if (randomPerson[1] !== userInput) {
-      setHelperText(
-        `False, the correct answer is ${randomPerson[1].toUpperCase()}.`
-      );
-      setAnswered(true);
-      resetCounter();
-    }
-  };
+	const handleSubmit = (event) => {
+		const { randomPerson, addCounter, resetCounter } = props;
+		event.preventDefault();
+		const userInput = value.toLowerCase();
+		if (answered === true) {
+			setTotalAnswers(totalAnswers + 1);
+			handleRefresh();
+			setAnswered(false);
+		} else if (randomPerson[1] === userInput) {
+			addCounter();
+			setCorrectAnswers(correctAnswers + 1);
+			setTotalAnswers(totalAnswers + 1);
+			// alert('Correct!')
+			handleRefresh();
+			setCorrect(true);
+			addStreak();
+		} else if (randomPerson[1] !== userInput) {
+			setHelperText(`False, the correct answer is ${randomPerson[1].toUpperCase()}.`);
+			setAnswered(true);
+			resetCounter();
+		}
+	};
 
-  const handleExample = event => {
-    const { data, randomVerb } = props;
-    const hablar = data.filter(verb => verb.infinitive === "hablar");
-    const hablarTense = hablar.filter(
-      verb => verb.tense_english === randomVerb.tense_english
-    );
-    const hablarMood = hablarTense.filter(
-      verb => verb.mood_english === randomVerb.mood_english
-    );
-    const hablarExample = hablarMood[0];
-    event.preventDefault();
-    setHelperText(
-      `Yo + Hablar + ${
-        randomVerb.tense_english
-      } = YO ${hablarExample.form_1s.toUpperCase()}`
-    );
-  };
+	const handleExample = (event) => {
+		const { data, randomVerb } = props;
+		const hablar = data.filter((verb) => verb.infinitive === 'hablar');
+		const hablarTense = hablar.filter((verb) => verb.tense_english === randomVerb.tense_english);
+		const hablarMood = hablarTense.filter((verb) => verb.mood_english === randomVerb.mood_english);
+		const hablarExample = hablarMood[0];
+		event.preventDefault();
+		setHelperText(`Yo + Hablar + ${randomVerb.tense_english} = YO ${hablarExample.form_1s.toUpperCase()}`);
+	};
 
-  const addAccent = event => {
-    event.preventDefault();
-    const accent = event.target.value;
-    setValue(value + accent);
-  };
+	const addAccent = (event) => {
+		event.preventDefault();
+		const accent = event.target.value;
+		setValue(value + accent);
+	};
 
-  const handleRefresh = () => {
-    const { randomize } = props;
-    setValue("");
-    setHelperText(null);
-    setCorrect(false);
-    randomize();
-  };
+	const handleRefresh = () => {
+		const { randomize } = props;
+		setValue('');
+		setHelperText(null);
+		setCorrect(false);
+		randomize();
+	};
 
-  const addStreak = () => {
-    const { count } = props;
-    if (count >= bestStreak) {
-      setBestStreak(bestStreak + 1);
-      if (bestStreak % 5 === 0) {
-        refReward.current.rewardMe();
-      }
-    }
-  };
-  const { randomPerson, randomVerb, count } = props;
-  const {
-    infinitive,
-    tense_english,
-    mood_english,
-    infinitive_english
-  } = randomVerb;
-  const buttonText =
-    randomPerson[1] !== value.toLowerCase() && answered
-      ? "Next verb"
-      : "Submit";
-  const percentage =
-    totalAnswers < 1 ? 0 : ((correctAnswers / totalAnswers) * 100).toFixed(0);
-  console.log(infinitive_english);
-  return (
-    <div>
-      <div className="verb-info-wrapper">
-        <div className="verb-streak">
-          <div className="current-best-streak">
-            <div className="streak">current streak:</div>
-            <div className="twenty-four">{count}</div>
-          </div>
-          <Reward ref={refReward} type="emoji">
-            <div className="current-best-streak">
-              <div className="streak">best streak:</div>
-              <div className="twenty-four">
-                {bestStreak} <span role="img" aria-label="salsa dancer" />
-              </div>
-            </div>
-          </Reward>
-          <div className="current-best-streak">
-            <div className="streak">percentage:</div>
-            <div className="twenty-four">{percentage}%</div>
-          </div>
-        </div>
-        <Info
-          infinitive={infinitive}
-          infinitive_english={infinitive_english}
-          tense_english={tense_english}
-          mood_english={mood_english}
-        />
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="input-section">
-          <Person randomPerson={randomPerson[0]} />
-          <input
-            type="text"
-            value={value}
-            placeholder="Enter conjugated verb..."
-            onChange={handleChange}
-            className="input"
-          />
-          <Checkmark correct={correct} />
-        </div>
-        <div className="text-under-input">
-          <AccentButtons addAccent={addAccent} />
-          <div
-            className="hover-text"
-            type="button"
-            role="button"
-            tabIndex={0}
-            onClick={handleExample}
-          >
-            Show example <i className="far fa-arrow-alt-circle-right" />
-          </div>
-        </div>
-        <div className="helper-text">
-          {helperText && <div>{helperText}</div>}
-          <button
-            className="submit-button"
-            type="submit"
-            onClick={handleSubmit}
-          >
-            {buttonText}
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+	const addStreak = () => {
+		const { count } = props;
+		if (count >= bestStreak) {
+			setBestStreak(bestStreak + 1);
+			if (bestStreak % 5 === 0) {
+				refReward.current.rewardMe();
+			}
+		}
+	};
+	const { randomPerson, randomVerb, count } = props;
+	const { infinitive, tense_english, mood_english, infinitive_english, verb_english } = randomVerb;
+	const buttonText = randomPerson[1] !== value.toLowerCase() && answered ? 'Next verb' : 'Submit';
+	const percentage = totalAnswers < 1 ? 0 : (correctAnswers / totalAnswers * 100).toFixed(0);
+	console.log(infinitive_english);
+	return (
+		<div>
+			<div className="verb-info-wrapper">
+				<div className="verb-streak">
+					<div className="current-best-streak">
+						<div className="streak">current streak:</div>
+						<div className="twenty-four">{count}</div>
+					</div>
+					<Reward ref={refReward} type="emoji">
+						<div className="current-best-streak">
+							<div className="streak">best streak:</div>
+							<div className="twenty-four">
+								{bestStreak} <span role="img" aria-label="salsa dancer" />
+							</div>
+						</div>
+					</Reward>
+					<div className="current-best-streak">
+						<div className="streak">percentage:</div>
+						<div className="twenty-four">{percentage}%</div>
+					</div>
+				</div>
+				<Info
+					infinitive={infinitive}
+					infinitive_english={infinitive_english}
+					tense_english={tense_english}
+					mood_english={mood_english}
+					verb_english={verb_english}
+				/>
+			</div>
+			<form onSubmit={handleSubmit}>
+				<div className="input-section">
+					<Person randomPerson={randomPerson[0]} />
+					<input
+						type="text"
+						value={value}
+						placeholder="Enter conjugated verb..."
+						onChange={handleChange}
+						className="input"
+					/>
+					<Checkmark correct={correct} />
+				</div>
+				<div className="text-under-input">
+					<AccentButtons addAccent={addAccent} />
+					<div className="hover-text" type="button" role="button" tabIndex={0} onClick={handleExample}>
+						Show example <i className="far fa-arrow-alt-circle-right" />
+					</div>
+				</div>
+				<div className="helper-text">
+					{helperText && <div>{helperText}</div>}
+					<button className="submit-button" type="submit" onClick={handleSubmit}>
+						{buttonText}
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 Input.propTypes = {
-  randomPerson: PropTypes.string,
-  addCounter: PropTypes.func,
-  resetCounter: PropTypes.func,
-  data: PropTypes.array,
-  randomVerb: PropTypes.object,
-  randomize: PropTypes.func,
-  count: PropTypes.number
+	randomPerson: PropTypes.string,
+	addCounter: PropTypes.func,
+	resetCounter: PropTypes.func,
+	data: PropTypes.array,
+	randomVerb: PropTypes.object,
+	randomize: PropTypes.func,
+	count: PropTypes.number
 };
 
 export default Input;
