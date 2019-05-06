1ab743a134fb4d1b4aadd7898df2507457ab66bc
Cody Brewer <brewer.cody.m@gmail.com>
2019-05-05 23:10:08 -0600

    style changes

--- src/components/Pages/PagesStyled.js
+++ src/components/Pages/PagesStyled.js
@@ -8,4 +8,9 @@ export const Words = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  font-family: Sofia;
+  h2 {
+    margin: 30px 30px;
+    font-size: 6rem;
+  }
 `;
