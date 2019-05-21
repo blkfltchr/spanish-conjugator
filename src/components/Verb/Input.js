7bf867fd1ccfa5d6be5c0f9e0345e40cf4813fbc
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-05-21 11:28:54 -0500

    Refactored Input.js

--- src/components/Verb/Input.js
+++ src/components/Verb/Input.js
@@ -4,59 +4,57 @@ import Person from './Person';
 import AccentButtons from './AccentButtons';
 import Checkmark from '../Checkmark/Checkmark';
 
-class Input extends Component {
-  render() {
-    const {
-      randomPerson,
-      value,
-      handleChange,
-      correct,
-      addAccent,
-      handleExample,
-      helperText,
-      handleSubmit,
-      buttonText
-    } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <div className="input-section">
-            <Person randomPerson={randomPerson[0]} />
-            <input
-              type="text"
-              defaultValue={value}
-              placeholder="Enter conjugated verb..."
-              onChange={handleChange}
-              className="input"
-            />
-            <Checkmark correct={correct} />
+function Input(props) {
+  const {
+    randomPerson,
+    value,
+    handleChange,
+    correct,
+    addAccent,
+    handleExample,
+    helperText,
+    handleSubmit,
+    buttonText
+  } = props;
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="input-section">
+          <Person randomPerson={randomPerson[0]} />
+          <input
+            type="text"
+            defaultValue={value}
+            placeholder="Enter conjugated verb..."
+            onChange={handleChange}
+            className="input"
+          />
+          <Checkmark correct={correct} />
+        </div>
+        <div className="text-under-input">
+          <AccentButtons addAccent={addAccent} />
+          <div
+            className="hover-text"
+            type="button"
+            role="button"
+            tabIndex={0}
+            onClick={handleExample}
+          >
+            Show example <i className="far fa-arrow-alt-circle-right" />
           </div>
-          <div className="text-under-input">
-            <AccentButtons addAccent={addAccent} />
-            <div
-              className="hover-text"
-              type="button"
-              role="button"
-              tabIndex={0}
-              onClick={handleExample}
-            >
-              Show example <i className="far fa-arrow-alt-circle-right" />
-            </div>
-          </div>
-          <div className="helper-text">
-            {helperText && <div>{helperText}</div>}
-            <button
-              className="submit-button"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              {buttonText}
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+        </div>
+        <div className="helper-text">
+          {helperText && <div>{helperText}</div>}
+          <button
+            className="submit-button"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            {buttonText}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 Input.propTypes = {
