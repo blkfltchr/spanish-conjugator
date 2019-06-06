2791b37b590eb90819cb1a692a20967dbc039895
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-06-06 17:46:32 -0500

    Months was inaccurate. Now fixed with nested for loop. Added comments explaining how it works

--- src/components/Dashboard/Charts/MonthChart/index.js
+++ src/components/Dashboard/Charts/MonthChart/index.js
@@ -13,97 +13,64 @@ import {
 } from 'recharts';
 
 function MonthlyChart() {
-  const [monthCorrect, setMonthCorrect] = useState([]);
-  const [monthTotal, setMonthTotal] = useState([]);
   const [monthData, setMonthData] = useState([0]);
-
-  // const monthData = [
-  //   {
-  //     name: moment()
-  //       .subtract(31, 'd')
-  //       .format('Do-MMM'),
-  //     correct: 40,
-  //     answers: 90
-  //   },
-  //   { name: '06/02/19', correct: 30, answers: 72 },
-  //   { name: '06/03/19', correct: 20, answers: 62 },
-  //   { name: '06/04/19', correct: 12, answers: 54 },
-  //   { name: '06/05/19', correct: 18, answers: 32 },
-  //   { name: '06/06/19', correct: 23, answers: 25 },
-  //   { name: '06/07/19', correct: 24, answers: 56 },
-  //   { name: '06/08/19', correct: 0, answers: 0 },
-  //   { name: '06/09/19', correct: 0, answers: 0 },
-  //   { name: '06/10/19', correct: 0, answers: 0 },
-  //   { name: '06/11/19', correct: 0, answers: 0 },
-  //   { name: '06/12/19', correct: 30, answers: 72 },
-  //   { name: '06/13/19', correct: 20, answers: 62 },
-  //   { name: '06/14/19', correct: 12, answers: 54 },
-  //   { name: '06/15/19', correct: 18, answers: 32 },
-  //   { name: '06/16/19', correct: 0, answers: 0 },
-  //   { name: '06/17/19', correct: 0, answers: 0 },
-  //   { name: '06/18/19', correct: 39, answers: 82 },
-  //   { name: '06/19/19', correct: 12, answers: 34 },
-  //   { name: '06/20/19', correct: 12, answers: 34 },
-  //   { name: '06/21/19', correct: 40, answers: 90 },
-  //   { name: '06/22/19', correct: 30, answers: 72 },
-  //   { name: '06/23/19', correct: 20, answers: 62 },
-  //   { name: '06/24/19', correct: 12, answers: 54 },
-  //   { name: '06/25/19', correct: 18, answers: 32 },
-  //   { name: '06/26/19', correct: 23, answers: 25 },
-  //   { name: '06/27/19', correct: 12, answers: 34 },
-  //   { name: '06/28/19', correct: 12, answers: 34 },
-  //   { name: '06/29/19', correct: 12, answers: 34 },
-  //   { name: '06/30/19', correct: 12, answers: 34 }
-  // ];
 
   const oneMonthAgo = moment()
     .subtract(31, 'd')
     .format('YYYY-MM-DD');
 
-  console.log('oneMonthAgo', oneMonthAgo, oneMonthAgo + 1);
-
-  const { loading, data } = useQuery(MY_LOGS_BY_DATE, {
+  const { data } = useQuery(MY_LOGS_BY_DATE, {
     variables: {
       date: oneMonthAgo
     }
   });
+
+  // we create an array of objects with the date and
+  // the count of correct and total answers on that date
+  // name is used for the graph
+  // dateForUseEffect is used for mapping in useEffect
   let arr = [];
-  // let days = 31;
-  let days = 31;
-  while (days > 0) {
+  let days = 30;
+  while (days >= 0) {
     arr.push({
       name: moment()
         .subtract(days, 'd')
         .format('Do-MMM'),
       correct: 0,
+      answers: 0,
+      dateForUseEffect: moment()
+        .subtract(days, 'd')
+        .format('D'),
+      correct: 0,
       answers: 0
     });
     days--;
   }
-  console.log('Will  it  work?', arr[2].correct, arr);
 
-  // getDate() returns 1-31; Sun-Sat
-  // we update the temp arrays based on the count of answers
-  // Sun-Sat and then setState with the updated array
+  // we map through the users logs for the month
+  // getDate() returns 1-31;
+  // we then map through the arr created above
+  // if the row in the above arr has the same date
+  // as the user log, we increase the total answers and
+  // correct (if correct)
+  // this is a nested for loop and there may be a more
+  // efficient way of doing this
+  // it currently only re-renders when data is updated
+  // if a user answers a question, they won't see the update
   useEffect(() => {
     if (Object.values(data).length > 0) {
-      // let tempWeekTotal = [0, 0, 0, 0, 0, 0, 0];
-      // let tempWeekCor = [0, 0, 0, 0, 0, 0, 0];
-      console.log("We're in use effect");
-      // let count = 0;
-      // let tempData = [];
       data.myLogs.map(val => {
-        console.log('What is the val----', val);
         const aDate = new Date(val.createdAt);
         const theDay = aDate.getDate();
-        console.log('A date + the Day', aDate, theDay);
-        console.log('Arr', arr);
-        arr[arr.length - 1 - theDay].answers += 1;
-        if (val.correct === true) {
-          arr[arr.length - 1 - theDay].correct += 1;
-        }
-        // setWeekCorrect(tempWeekCor);
-        // setWeekTotal(tempWeekTotal);
+
+        arr.map(row => {
+          if (parseInt(row.dateForUseEffect) === theDay) {
+            row.answers += 1;
+            if (val.correct === true) {
+              row.correct += 1;
+            }
+          }
+        });
         setMonthData(arr);
       });
     }
